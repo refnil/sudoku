6eddc4e891e4a3f68235e01c7f4ea8bad1824b43
Martin Lavoie <broemartino@gmail.com>
2021-05-01 12:50:42 +0200

    check thermo during solve

--- src/solver/variant.rs
+++ src/solver/variant.rs
@@ -1,10 +1,8 @@
- 
-use std::fmt::{Write};
-                        
-use std::collections::HashMap;
 use crate::board::{Sudoku, Variant};
 use crate::helper::Unsolvable;
-use crate::solver::{mask_iter, Solutions, Solver, SudokuSolver, Guess, OutsideSolver};
+use crate::solver::{mask_iter, Guess, OutsideSolver, Solutions, Solver, SudokuSolver};
+use std::collections::HashMap;
+use std::fmt::Write;
 
 #[derive(Clone, Copy)]
 struct Thermo {
@@ -12,18 +10,19 @@ struct Thermo {
 }
 
 struct ThermoShared {
-    thermo_info: HashMap<u32, ThermoSharedNode>,
+    thermo_info: Vec<HashMap<u32, ThermoSharedNode>>,
     thermo_init: [u32; 27],
 }
 
 struct ThermoSharedNode {
+    // value in normal number 0 <= number < 81
+    //value: u32,
     smaller: [u32; 3],
     bigger: [u32; 3],
 }
 
-
 impl Thermo {
-    fn from_thermos(ts: &Vec<Vec<u32>>) -> Option<(Thermo, ThermoShared)>  {
+    fn from_thermos(ts: &Vec<Vec<u32>>) -> Option<(Thermo, ThermoShared)> {
         if ts.is_empty() {
             return None;
         }
@@ -33,67 +32,73 @@ impl Thermo {
         };
 
         let mut shared = ThermoShared {
-            thermo_info: HashMap::new(),
+            thermo_info: vec![HashMap::new(), HashMap::new(), HashMap::new()],
             thermo_init: [!0; 27],
         };
 
-        fn apply_or(s: &mut [u32; 3], o: &[u32;3]) {
+        fn apply_or(s: &mut [u32; 3], o: &[u32; 3]) {
             for i in 0..3 {
                 s[i] |= o[i];
             }
         }
 
-        for t in ts{
+        for t in ts {
             let mut smaller = [0; 3];
-            let mut bigger = [0;3];
+            let mut bigger = [0; 3];
 
             let length = t.len();
 
             for (i, c) in t.iter().enumerate() {
                 // Add to smaller part of the thermo node
-                shared.thermo_info.entry(*c)
-                    .and_modify(|n| apply_or(&mut n.smaller, &smaller))
-                    .or_insert(ThermoSharedNode {
-                        smaller, bigger
-                    });
                 let band = (c / 27) as usize;
                 let pos = c % 27;
-                smaller[band] |= 1 << pos;
+                let key = 1 << pos;
+                shared.thermo_info[band]
+                    .entry(key)
+                    .and_modify(|n| apply_or(&mut n.smaller, &smaller))
+                    .or_insert(ThermoSharedNode {
+                        //value: *c,
+                        smaller,
+                        bigger,
+                    });
+                smaller[band] |= key;
 
                 // cell cell as in thermo
                 thermo.mask_in_thermo[band] |= 1 << pos;
 
                 // Remove possibilities from starting grid
                 // Remove number too small
-                println!("For cell: {} of length {}", i, length);
+                // println!("For cell: {} of length {}", i, length);
                 for j in 0..i {
-                    println!("removing {} because too small", j);
+                    //println!("removing {} because too small", j);
                     shared.thermo_init[band + j * 3] &= !(1 << pos);
                 }
                 // Remove number too big
-                for j in (9-length+i+1)..9 {
-                    println!("removing {} because too big", j);
+                for j in (9 - length + i + 1)..9 {
+                    //println!("removing {} because too big", j);
                     shared.thermo_init[band + j * 3] &= !(1 << pos);
                 }
             }
 
             for c in t.iter().rev() {
                 // Add to bigger part of the thermo node
-                shared.thermo_info.entry(*c).and_modify(|n| apply_or(&mut n.bigger, &bigger));
-                let band = c / 27;
+                let band = (c / 27) as usize;
                 let pos = c % 27;
-                bigger[band as usize] |= 1 << pos;
+                let key = 1 << pos;
+                shared.thermo_info[band]
+                    .entry(key)
+                    .and_modify(|n| apply_or(&mut n.bigger, &bigger));
+                bigger[band] |= key;
             }
         }
-
 
         Some((thermo, shared))
     }
 }
 
 pub(crate) struct VariantSolver {
-   constant: VariantSolverConstant,
-   copy : VariantSolverCopy,
+    constant: VariantSolverConstant,
+    copy: VariantSolverCopy,
 }
 
 struct VariantSolverConstant {
@@ -103,13 +108,13 @@ struct VariantSolverConstant {
     thermo: Option<ThermoShared>,
 }
 
-#[derive(Clone,Copy)]
-struct VariantSolverCopy{
+#[derive(Clone, Copy)]
+struct VariantSolverCopy {
     base: SudokuSolver,
     thermo: Option<Thermo>,
 }
 
-#[derive(Clone,Copy)]
+#[derive(Clone, Copy)]
 struct VariantSolverCopyPlus<'a> {
     constant: &'a VariantSolverConstant,
     base: VariantSolverCopy,
@@ -164,11 +169,11 @@ impl<'a> Solver for VariantSolverCopyPlus<'a> {
         self.base.base.extract_solution()
     }
 
-    fn insert_candidate_by_mask(&mut self, guess: &Guess){
+    fn insert_candidate_by_mask(&mut self, guess: &Guess) {
         self.base.base.insert_candidate_by_mask(guess)
     }
 
-    fn remove_candidate_by_mask(&mut self, guess: &Guess){
+    fn remove_candidate_by_mask(&mut self, guess: &Guess) {
         self.base.base.remove_candidate_by_mask(guess)
     }
 }
@@ -177,8 +182,7 @@ impl VariantSolver {
     pub fn from_variant(variant: Variant) -> Result<Self, Unsolvable> {
         let (t_c, t_s) = if let Some((c, s)) = Thermo::from_thermos(&variant.thermo) {
             (Some(c), Some(s))
-        } 
-        else {
+        } else {
             (None, None)
         };
         let constant = VariantSolverConstant {
@@ -189,11 +193,11 @@ impl VariantSolver {
         };
         let copy = VariantSolverCopy {
             base: SudokuSolver::from_sudoku(variant.base)?,
-            thermo: t_c, 
+            thermo: t_c,
         };
         let final_copy = {
             let mut plus = VariantSolverCopyPlus {
-                constant : &constant,
+                constant: &constant,
                 base: copy,
             };
             plus.init_thermo();
@@ -202,7 +206,7 @@ impl VariantSolver {
             if !plus.is_ok_variants() {
                 println!("Invalid from parse");
                 return Err(Unsolvable);
-            } 
+            }
             plus.base
         };
 
@@ -236,11 +240,15 @@ impl<'a> VariantSolverCopyPlus<'a> {
         if self.has_king() {
             self.check_king()?
         }
+        self.check_thermo();
         Ok(())
     }
 
     fn is_ok_variants(&self) -> bool {
-        (!self.has_diag_pos() || self.is_ok_diag_pos()) && (!self.has_diag_neg() || self.is_ok_diag_neg()) && (!self.has_king() || self.is_ok_king()) && self.is_ok_thermo()
+        (!self.has_diag_pos() || self.is_ok_diag_pos())
+            && (!self.has_diag_neg() || self.is_ok_diag_neg())
+            && (!self.has_king() || self.is_ok_king())
+            && self.is_ok_thermo()
     }
 
     fn is_ok_diag_pos(&self) -> bool {
@@ -391,7 +399,6 @@ impl<'a> VariantSolverCopyPlus<'a> {
             let band2 = base.poss_cells[1 + 3 * number] & !base.unsolved_cells[1];
             let band3 = base.poss_cells[2 + 3 * number] & !base.unsolved_cells[2];
 
-
             if compare_line(band1, band1 >> 9) {
                 return false;
             }
@@ -428,7 +435,7 @@ impl<'a> VariantSolverCopyPlus<'a> {
     }
 
     fn check_king(&mut self) -> Result<(), Unsolvable> {
-        fn get_mask(mask_offset: u32, mut other_line: u32) -> u32{
+        fn get_mask(mask_offset: u32, mut other_line: u32) -> u32 {
             other_line &= 511;
 
             let invalid_value = ((other_line >> 1) | (other_line << 1)) & 511;
@@ -487,40 +494,79 @@ impl<'a> VariantSolverCopyPlus<'a> {
     }
 
     fn check_thermo(&mut self) {
+        if let Some(t) = &self.base.thermo {
+            let unsolved = &self.base.base.unsolved_cells;
+            let poss_cells = &mut self.base.base.poss_cells;
+
+            for band in 0..3 {
+                let band = band as usize;
+                for pos in mask_iter(!unsolved[band] & t.mask_in_thermo[band]) {
+                    let node = self.constant.thermo.as_ref().unwrap().thermo_info[band]
+                        .get(&pos)
+                        .unwrap();
+
+                    let mut pos_to_remove = &node.bigger;
+                    for n in 0..9 {
+                        if (poss_cells[band + n * 3] & pos) != 0 {
+                            // Found the added number to the thermo
+                            pos_to_remove = &node.smaller;
+                            for band_2 in 0..3 {
+                                poss_cells[band_2 + n * 3] &= !node.bigger[band_2] | !node.smaller[band_2];
+                            }
+                        } else {
+                            for band_2 in 0..3 {
+                                poss_cells[band_2 + n * 3] &= !pos_to_remove[band_2];
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     fn is_ok_thermo(&self) -> bool {
         if let Some(t) = &self.constant.thermo {
             let pos_cells = &self.base.base.poss_cells;
             let unsolved = &self.base.base.unsolved_cells;
-            for (key, val) in t.thermo_info.iter() {
-                let mut hit_cell = false;
-                let mut hit_cell_number = 55;
-                let band = (key / 27) as usize;
-                let pos = key % 27;
+            for band in 0..3 {
+                let band = band as usize;
+                for (key, val) in t.thermo_info[band].iter() {
+                    let mut hit_cell = false;
+                    let mut hit_cell_number = 55;
 
-                for n in 0..9 {
-                    let n_band_offset = (n * 3) as usize;
-                    let cur_hit_cell = (pos_cells[band + n_band_offset] & !unsolved[band] & 1 << pos) != 0;
-                    hit_cell |= cur_hit_cell;
-                    if cur_hit_cell {
-                        hit_cell_number = n+1;
-                    }
-                    let mut cur_hit_small = false;
-                    let mut s = String::new();
-                    //let mut cur_hit_big = false;
-                    for b in 0..3 {
-                        let bi = b as usize;
-                        let pci = bi + n_band_offset;
-                        writeln!(&mut s, "{:#032b}", (pos_cells[pci] & !unsolved[b] & val.smaller[bi]));
-                        cur_hit_small |= (pos_cells[pci] & !unsolved[b] & val.smaller[bi]) != 0;
-                        //cur_hit_big |= (pos_cells[pci] & val.bigger[bi]) != 0;
-                    }
-                    if hit_cell && cur_hit_small {
-                        print!("{}", s);
-                        println!("n {} key {} cur_hit {} hit_number {}", n+1, key, cur_hit_cell, hit_cell_number); 
-                        println!("{}", self.base.base.extract_solution().display_block());
-                        return false;
+                    for n in 0..9 {
+                        let n_band_offset = (n * 3) as usize;
+                        let cur_hit_cell = (pos_cells[band + n_band_offset] & !unsolved[band] & key) != 0;
+                        hit_cell |= cur_hit_cell;
+                        if cur_hit_cell {
+                            hit_cell_number = n + 1;
+                        }
+                        let mut cur_hit_small = false;
+                        let mut s = String::new();
+                        for b in 0..3 {
+                            let bi = b as usize;
+                            let pci = bi + n_band_offset;
+                            writeln!(
+                                &mut s,
+                                "{:#032b}",
+                                (pos_cells[pci] & !unsolved[b] & val.smaller[bi])
+                            );
+                            cur_hit_small |= (pos_cells[pci] & !unsolved[b] & val.smaller[bi]) != 0;
+                        }
+                        if hit_cell && cur_hit_small {
+                            /*
+                            print!("{}", s);
+                            println!(
+                                "n {} key {} cur_hit {} hit_number {}",
+                                n + 1,
+                                key,
+                                cur_hit_cell,
+                                hit_cell_number
+                            );
+                            println!("{}", self.base.base.extract_solution().display_block());
+                            */
+                            return false;
+                        }
                     }
                 }
             }
